42fd2afa86a82be1d6650b69877483e1899fe1aa
Ayala3423 <a0548573423@gmail.com>
2024-12-20 00:42:58 +0200

    login css

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,10 @@
-import { useState, useRef, useEffect} from 'react'
+import { useState, useRef, useEffect } from 'react'
 import './Login.css'
 import { BrowserRouter as Router, Route, Routes, Link, useNavigate, useLocation } from "react-router-dom";
 
 export default function Login() {
-    const fieldsRef = useRef({})
-    const navigate=useNavigate();
+    const fieldsRef = useRef({});
+    const navigate = useNavigate();
     let users;
     useEffect(() => {
         fetch('http://localhost:3000/users')
@@ -15,7 +15,7 @@ export default function Login() {
     const handleSubmit = (e) => {
         const name = fieldsRef.current.name.value;
         const password = fieldsRef.current.password.value;
-        const foundUser= users.find(user => user.username === name && user.website === password);
+        const foundUser = users.find(user => user.username === name && user.website === password);
         if (!foundUser) {
             alert('one or more of the details is incorrect');
         }
@@ -24,30 +24,36 @@ export default function Login() {
             navigate(`/home/users/${foundUser.id}`);
         }
     }
+
     return (
         <>
-            <main>
-                <div className="textfield">
-                    <label htmlFor="user">User</label>
-                    <input
-                        type="text"
-                        name="user"
-                        placeholder="User"
-                        required
-                        ref={(el) => (fieldsRef.current["name"] = el)}
-                    />
+            <main className='main-login'>
+                <div className="right-login">
+                    <div className="card-login">
+                        <h1>Login</h1>
+                        <div className="textfield">
+                            <label htmlFor="user">User</label>
+                            <input
+                                type="text"
+                                name="user"
+                                placeholder="User"
+                                required
+                                ref={(el) => (fieldsRef.current["name"] = el)}
+                            />
+                        </div>
+                        <div className="textfield">
+                            <label htmlFor="password">Password</label>
+                            <input
+                                type="password"
+                                name="password"
+                                placeholder="Password"
+                                required
+                                ref={(el) => (fieldsRef.current["password"] = el)}
+                            />
+                        </div>
+                        <button className="btn-login" type="submit" onClick={handleSubmit}>Login</button>
+                    </div>
                 </div>
-                <div className="textfield">
-                    <label htmlFor="password">Password</label>
-                    <input
-                        type="password"
-                        name="password"
-                        placeholder="Password"
-                        required
-                        ref={(el) => (fieldsRef.current["password"] = el)}
-                    />
-                </div>
-                <button className="btn-login" type="submit" onClick={handleSubmit}>Login</button>
             </main>
         </>
     )
